f1c4929d473ba2524354e8ed814123e797ec2823
marycriv <maryecrivelli@gmail.com>
2022-02-26 20:47:26 -0500

    status msg added to login resp

--- src/actions/login.js
+++ src/actions/login.js
@@ -2,21 +2,29 @@ export const login = async (details) => {
     const API = "http://localhost:3010/api/v1/"
 
     const params = {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ user: details })
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: details })
     };
 
-    const response = await fetch(API + "login", params);
+    let response = await fetch(API + "login", params)
     const data = await response.json();
     const loginInfo = data;
 
-    const response3 = await fetch(API + "pressings");
-    const data3 = await response3.json();
-    const pressingInfo = data3.data;
+    const response2 = await fetch(API + "pressings");
+    const data2 = await response2.json();
+    const pressingInfo = data2.data;
 
-    return { loginInfo, pressingInfo }
+    if (loginInfo.status == 200) {
+      return { 
+          loginInfo, 
+          pressingInfo 
+      }
+    } else {
+      console.log("error!!!!") 
+    }
+    
 };
